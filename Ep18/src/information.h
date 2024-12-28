6fb020a55c26ef0f1d6f7724b31b706284faa29a
subodh-info <subodhkr0692@gmail.com>
2024-12-28 14:23:24 +0530

    example for organizing project files

---
+++ Ep18/src/information.h
@@ -0,0 +1,11 @@
+#ifndef INFORMATION_H
+#define INFORMATION_H
+
+#include <iostream>
+
+namespace Logging
+{
+    std::string getOsName();
+}
+
+#endif
---
+++ Ep18/src/math/supermath.cpp
@@ -0,0 +1,6 @@
+#include "include/supermath.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
